2248973561223c0f39abc6762c8de58bb51a9cc7
Carlos Tec <221k0028@itscarrillopuerto.edu.mx>
2026-02-10 11:18:16 -0500

    Corrección de conexión db.php

--- db.php
+++ db.php
@@ -1,13 +1,14 @@
 <?php
 
-$host = "sql200.infinityfree.com"; // Sustituye XXX por el número que te den
-$user = "if0_41118946";           // Tu usuario de la cuenta
-$pass = " tuxmSfeOTwmvK";     // La contraseña que configuraste o te asignaron
-$db   = "if0_41118946_XXX"; // ¡Ojo! Aquí el nombre de la DB suele llevar un prefijo
+$host = "sql200.infinityfree.com";
+$user = "if0_41118946";
+$pass = "tuxmSfeOTwmvK";
+$db   = "if0_41118946_XXX"; 
 
 $conexion = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conexion) {
     die("Fallo la conexión: " . mysqli_connect_error());
 }
+
 ?>
